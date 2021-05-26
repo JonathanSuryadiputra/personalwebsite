49d9115f23e69efa66db61f455a3c222725a0de9
Jonathan Suryadiputra <jsuryadiputra@gmail.com>
2021-05-26 22:00:04 +0100

    Added titles for the web pages for the mobile version of the website

--- mobile/about.php
+++ mobile/about.php
@@ -1,5 +1,6 @@
 <html>
 	<head>
+		<title>About Me - Jonathan Suryadiputra</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel='stylesheet' href='https://cdn.iconmonstr.com/1.3.0/css/iconmonstr-iconic-font.min.css'>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
